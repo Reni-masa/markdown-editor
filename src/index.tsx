e4c101a2c99035524aee093f3a4dbf9830e49134
SugitaMasataka <mstk.ambit@gmail.com>
2021-02-11 16:09:56 +0900

    dist push対象許可

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 // console.log('hoge');
-const Main = (<h1>Markdown Editor</h1>);
+const Main = (<h1>Markdown Editor react</h1>);
 
 render(Main, document.getElementById('app'));
