bfd0568c2a1f6a89c7f346c581fa8015a1a8f213
SugitaMasataka <mstk.ambit@gmail.com>
2021-02-11 15:55:12 +0900

    react install

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,21 @@ const path = require('path');
 
 module.exports = {
   //   entry: './src/index.js',
-  entry: './src/index.ts',
+  //   entry: './src/index.ts',
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        // test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: ['.ts']
+    // extensions: ['.ts']
+    extensions: ['.js', '.ts', '.tsx']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
